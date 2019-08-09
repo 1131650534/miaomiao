1715a937ede5d1f4a079bd5b75c267eaca7c16e6
1131650534@qq.com <1131650534@qq.com>
2019-08-09 13:51:58 +0800

    详情页开发

--- src/views/Movie/detail.vue
+++ src/views/Movie/detail.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="detailContrainer" class="animation">
-  
     <Header title="影片详情">
-        <i class="iconfont icon-right" @touchstart="handleToBack"></i>
+      <i class="iconfont icon-right" @touchstart="handleToBack"></i>
     </Header>
-    <div id="content" class="contentDetail">
-      <div class="detail_list">
+    <Loading v-if="isLoading"/>
+    <div v-else id="content" class="contentDetail">
+      <!-- <div class="detail_list">
         <div class="detail_list_bg"></div>
         <div class="detail_list_filter"></div>
         <div class="detail_list_content">
@@ -34,37 +34,33 @@
             <p>陈建斌</p>
             <p>马先勇</p>
           </li>
-          <li class="swiper-slide">
+        </ul>
+      </div>-->
+      <div class="detail_list">
+        <div class="detail_list_bg"></div>
+        <div class="detail_list_filter"></div>
+        <div class="detail_list_content">
+          <div class="detail_list_img">
+            <img :src="detailMovie.img|setWH('148.208')" />
+          </div>
+          <div class="detail_list_info">
+            <h2>{{detailMovie.nm}}</h2>
+            <p>{{detailMovie.enm}}</p>
+            <p>{{detailMovie.sc}}</p>
+            <p>{{detailMovie.cat}}</p>
+            <p>{{detailMovie.src}} / {{detailMovie.dur}}</p>
+            <p>{{detailMovie.pubDesc}}</p>
+          </div>
+        </div>
+      </div>
+      <div class="detail_intro">
+        <p>{{detailMovie.dra}}</p>
+      </div>
+      <div class="detail_player swiper-container" ref="detail_plater">
+        <ul class="swiper-wrapper">
+          <li v-for="(item,index) in detailMovie.photos" :key="index" class="swiper-slide">
             <div>
-              <img src="/images/person_1.webp" alt />
-            </div>
-            <p>陈建斌</p>
-            <p>马先勇</p>
-          </li>
-          <li class="swiper-slide">
-            <div>
-              <img src="/images/person_1.webp" alt />
-            </div>
-            <p>陈建斌</p>
-            <p>马先勇</p>
-          </li>
-          <li class="swiper-slide">
-            <div>
-              <img src="/images/person_1.webp" alt />
-            </div>
-            <p>陈建斌</p>
-            <p>马先勇</p>
-          </li>
-          <li class="swiper-slide">
-            <div>
-              <img src="/images/person_1.webp" alt />
-            </div>
-            <p>陈建斌</p>
-            <p>马先勇</p>
-          </li>
-          <li class="swiper-slide">
-            <div>
-              <img src="/images/person_1.webp" alt />
+              <img :src="item|setWH('140.127')" />
             </div>
             <p>陈建斌</p>
             <p>马先勇</p>
@@ -76,21 +72,40 @@
 </template>
 
 <script>
-import Header from '@/components/header';
+import Header from "@/components/header";
 export default {
   name: "Detail",
-  components:{
-      Header
+  data() {
+    return {
+      detailMovie: {},
+      isLoading:true
+    };
+  },
+  components: {
+    Header
   },
   //接收movieId
-  props:['movieId'],
-  methods:{
-      handleToBack(){
-          this.$router.back();
-      }
+  props: ["movieId"],
+  methods: {
+    handleToBack() {
+      this.$router.back();
+    }
   },
-  mounted(){
-    console.log(this.movieId)
+  mounted() {
+    this.axios.get("/api/detailmovie?movieId=" + this.movieId).then(res => {
+      var msg = res.data.msg;
+      this.isLoading=false;
+      if (msg === "ok") {
+        this.detailMovie = res.data.data.detailMovie;
+        this.$nextTick(() => {
+          new Swiper(this.$refs.detail_plater, {
+            slidesPerView: "auto",
+            freeMode: true,
+            freeModeSticky: true
+          });
+        });
+      }
+    });
   }
 };
 </script>
@@ -105,12 +120,16 @@ export default {
   min-height: 100%;
   background: white;
 }
-#detailContrainer.animation{
-  animation:.3s slideMove;
+#detailContrainer.animation {
+  animation: 0.3s slideMove;
 }
 @keyframes slideMove {
-  0%{transform: translatex(100%);}
-  100%{transform: translatex(0%);}
+  0% {
+    transform: translatex(100%);
+  }
+  100% {
+    transform: translatex(0%);
+  }
 }
 #content.contentDetail {
   display: block;
@@ -176,5 +195,24 @@ export default {
   line-height: 20px;
   font-size: 14px;
   color: #ccc;
+}
+#content .detail_intro {
+  padding: 10px;
+}
+#content .detail_player {
+  margin: 20px;
+}
+.detail_player .swiper-slide {
+  width: 70px;
+  margin-right: 20px;
+  text-align: center;
+  font-size: 14px;
+}
+.detail_player .swiper-slide img {
+  width: 100%;
+  margin-bottom: 5px;
+}
+.detail_player .swiper-slide p:nth-of-type(2) {
+  color: #999;
 }
 </style>
